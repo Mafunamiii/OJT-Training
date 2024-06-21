dc4445f0fb2eaa58309dd2e64a6c9c0fcb572c2d
Jahnreil Amarillento <92299867+Mafunamiii@users.noreply.github.com>
2024-06-21 17:55:00 +0800

    Added SearchBook
    SearchBook in progress

--- mini-project-2/src/main/java/org/example/ManageBook.java
+++ mini-project-2/src/main/java/org/example/ManageBook.java
@@ -1,6 +1,9 @@
 package org.example;
 
+import javax.swing.*;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class ManageBook {
     static ArrayList<Book> bookList = new ArrayList<Book>();
@@ -12,32 +15,93 @@ public class ManageBook {
         bookList.add(new Book(title,isbn));
     }
 
+    public void addBook(String title,String author, String isbn) {
+        bookList.add(new Book(title, author, isbn));
+    }
+
     public void addHardBack(Book book, int pagecount) {
-        bookList.add(new Book.hardBack(book.getTitle(),book.getISBN(),pagecount));
+        bookList.add(new Book.hardBack(
+                book.getTitle(),
+                book.getISBN(),
+                pagecount
+        ));
     }
 
-    static void addHardBack(Book book, String coverType, int pageCount, double weightKG) {
-
+    public void addHardBack(Book book, String coverType, int pageCount, double weightKG) {
+        bookList.add(new Book.hardBack(
+                book.getTitle(),
+                book.getAuthor(),
+                book.getISBN(),
+                book.getGenre(),
+                book.getPublisher(),
+                book.getPublicationDate(),
+                book.getSynopsis(),
+                book.getLanguage(),
+                coverType,
+                pageCount,
+                weightKG
+        ));
     }
 
-    static void addEBook(Book book, String fileFormat, double fileSize ) {
-
+    public void addEBook(Book book, String fileFormat, double fileSize) {
+        bookList.add(new Book.eBook(
+                book.getTitle(),
+                book.getAuthor(),
+                book.getISBN(),
+                book.getGenre(),
+                book.getPublisher(),
+                book.getPublicationDate(),
+                book.getSynopsis(),
+                book.getLanguage(),
+                fileFormat,
+                fileSize
+        ));
     }
 
-    static void addEBook(Book book, String fileFormat) {
-
+    public void addEBook(Book book, String fileFormat) {
+        bookList.add(new Book.eBook(
+                book.getTitle(),
+                book.getISBN(),
+                fileFormat
+        ));
     }
 
-    static void addAudioBook(Book book, String fileFormat, double fileSize, String audioFormat, String bitrate) {
 
+
+    public void addAudioBook(Book book, String fileFormat, double fileSize, String audioFormat, String bitrate) {
+        bookList.add(new Book.audioBook(
+                book.getTitle(),
+                book.getAuthor(),
+                book.getISBN(),
+                book.getGenre(),
+                book.getPublisher(),
+                book.getPublicationDate(),
+                book.getSynopsis(),
+                book.getLanguage(),
+                fileFormat,
+                fileSize,
+                audioFormat,
+                bitrate
+        ));
     }
 
-    static void addAudioBook(Book book, String fileFormat, String audioFormat, String bitrate) {
-
+    public void addAudioBook(Book book, String fileFormat, String audioFormat, String bitrate) {
+        bookList.add(new Book.audioBook(
+                book.getTitle(),
+                book.getISBN(),
+                fileFormat,
+                audioFormat,
+                bitrate
+        ));
     }
 
     static void addAudioBook(Book book, String fileFormat, String audioFormat) {
-
+        bookList.add(new Book.audioBook(
+                book.getTitle(),
+                book.getISBN(),
+                fileFormat,
+                audioFormat
+        ));
     }
 
     // ==========================================
@@ -48,24 +112,102 @@ public class ManageBook {
 
     // ==========================================
 
-    static void searchBook() {
+    public void searchBook(ArrayList<Book> bookList) {
+        ArrayList<Book> results = new ArrayList<>();
+        while (true) {
+            System.out.print(
+                    "\n============== Search by? ==============\n" +
+                            "            [1] Title\n"+
+                            "            [2] Author\n"+
+                            "            [3] ISBN\n"+
+                            "            [4] Genre\n"+
+                            "            [5] Publisher\n"+
+                            "            [6] Language\n►");
+            String input = Main.inScanner.nextLine();
+            int choice = Main.inputCheck(input, 6);
+
+            switch (choice) {
+                case 1:
+                    System.out.print("\nEnter Title: ");
+                    String title = Main.inScanner.nextLine();
+                    for (Book book: bookList) {
+                        if (book.getTitle().equalsIgnoreCase(title)) {
+                            results.add(book);
+                        }
+                    }
+                    break;
+                case 2:
+                    System.out.print("\nEnter Author: ");
+                    String author = Main.inScanner.nextLine();
+                    for (Book book: bookList) {
+                        if (book.getAuthor().equalsIgnoreCase(author)) {
+                            results.add(book);
+                        }
+                    }
+                    break;
+                case 3:
+                    System.out.print("\nEnter ISBN: ");
+                    String isbn = Main.inScanner.nextLine();
+                    for (Book book: bookList) {
+                        if (book.getISBN().equalsIgnoreCase(isbn)) {
+                            results.add(book);
+                        }
+                    }
+                    break;
+                case 4:
+                    System.out.print("\nEnter Genre: ");
+                    String genre = Main.inScanner.nextLine();
+                    for (Book book: bookList) {
+                        if (book.getGenre().equalsIgnoreCase(genre)) {
+                            results.add(book);
+                        }
+                    }
+                    break;
+                case 5:
+
+                    break;
+                case 6:
+
+                    break;
+                default:
+                    System.out.println("Invalid input, Select from the choices above");
+            }
+        }
 
     }
+
+    static ArrayList<Book> searchBookTitle(String title) {
+        ArrayList<Book> resultsTitle = new ArrayList<>();
+        for (Book book: bookList) {
+            if (book.getTitle().equalsIgnoreCase(title)) {
+                resultsTitle.add(book);
+            }
+        }
+        return resultsTitle;
+    }
+
 
     // ==========================================
     static ArrayList<Book> getBookList() {
         return bookList;
     }
 
-    static void retrieveBooks() {
+    static void retrieveBooks(ArrayList<Book> bookList) {
         int pageSize = 5;
         int currentPage = 1;
         int catalogPage = 0;
         int targetPage = pageSize - 1;
 
         while (true) {
-            System.out.println("\n\n========= Book Catalog =========\n" +
-                    "Title            || Author/s           || ISBN                      ");
+            System.out.println("\n\n============================================= Book Catalog =============================================");
+            System.out.println(String.format(
+                    "%-45s || %-30s || %-15s",
+                    "Title",
+                    "Author",
+                    "ISBN"
+                )
+            );
+            System.out.println("========================================================================================================");
 
             if (targetPage >= bookList.size()) {
                 targetPage = bookList.size() - 1;
@@ -77,14 +219,20 @@ public class ManageBook {
                     if (book.getAuthor() == null) {
                         book.setAuthor("Not Indicated");
                     }
-                    System.out.println(book.getTitle() + " || " + book.getAuthor() + "     ||    " + book.getISBN());
+                    System.out.println(String.format(
+                            "%-45s || %-30s || %-15s",
+                            book.getTitle(),
+                            book.getAuthor(),
+                            book.getISBN()
+                            )
+                    );
                 }
             } else {
 
                 System.out.println("No books to display on this page.");
             }
 
-            System.out.println("==================== Page " + currentPage + " =====================\n" +
+            System.out.println("================================================= Page " + currentPage + " ===============================================\n" +
                     "[1] View next page\n" +
                     "[2] View previous page\n" +
                     "[3] Back to Main Menu");
