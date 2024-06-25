44f807076fc9c8c61b8768743ad6bdee24677fc0
Jahnreil Amarillento <92299867+Mafunamiii@users.noreply.github.com>
2024-06-26 02:01:19 +0800

    Update input result

--- mini-project-2/src/main/java/com/jahnreil_stratpoint/InputResult.java
+++ mini-project-2/src/main/java/com/jahnreil_stratpoint/InputResult.java
@@ -3,26 +3,41 @@ package com.jahnreil_stratpoint;
 public class InputResult {
     public int valueInt;
     public boolean isValid;
+    public double valueDouble;
 
-    public InputResult(int value, boolean isValid) {
-        this.valueInt=value;
+    public InputResult(int valueint, boolean isValid, double valuedouble) {
+        this.valueInt=valueint;
         this.isValid=isValid;
+        this.valueDouble=valuedouble;
     }
 
-    public static InputResult inputCheck (String input, int maxChoice) {
-        int inputvalue = 0;
+    public static InputResult inputCheck (String input, int maxChoice, int inputType) {
+        int inputvalueInt = 0;
         boolean isValid = false;
+        double inputvalueDouble = 0;
 
         try {
             if (input.isEmpty()) {
                 throw new IllegalArgumentException("Input cannot be empty");
             }
-            inputvalue = Integer.parseInt(input);
-            if (inputvalue < 0 || inputvalue > maxChoice) {
-                System.out.println("Invalid Input");
-            } else {
-                isValid = true;
+
+            switch (inputType) {
+                case 1: //String input to integer with max value
+                    inputvalueInt = Integer.parseInt(input);
+                    if (inputvalueInt < 0 || inputvalueInt > maxChoice) {
+                        System.out.println("Invalid Input");
+                    } else {
+                        isValid = true;
+                    }
+                    break;
+                case 2: //String input to integer
+                    inputvalueInt = Integer.parseInt(input);
+                    break;
+                case 3: //String input to double
+                    inputvalueDouble = Double.parseDouble(input);
+                    break;
             }
+
         } catch (NumberFormatException e) {
             System.out.println("Invalid Input, select from the choices");
         } catch (IllegalArgumentException e) {
@@ -31,7 +46,7 @@ public class InputResult {
             System.out.println("Unexpected error: " + e.getMessage());
             e.printStackTrace();
         }
-
-        return new InputResult(inputvalue, isValid);
+        return new InputResult(inputvalueInt, isValid, inputvalueDouble);
     }
+
 }
