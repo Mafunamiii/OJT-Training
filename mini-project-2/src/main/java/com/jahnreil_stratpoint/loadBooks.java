42cdcb865daa3b47689ad7b4f596d94640d19dd1
Jahnreil Amarillento <92299867+Mafunamiii@users.noreply.github.com>
2024-06-26 02:01:38 +0800

    Loaded data from csv
    load data from csv rather than hardcoded

--- mini-project-2/src/main/java/com/jahnreil_stratpoint/loadBooks.java
+++ mini-project-2/src/main/java/com/jahnreil_stratpoint/loadBooks.java
@@ -1,5 +1,8 @@
 package com.jahnreil_stratpoint;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class loadBooks {
@@ -11,76 +14,80 @@ public class loadBooks {
         this.managebook = managebooks;
     }
 
-    public ArrayList<Book> loadfromCSV () {
+    public ArrayList<Book> loadFromCSV() {
+        String initialDirectory = "src/main/java/com/jahnreil_stratpoint/dataCSV/";
+        String[] filePaths = {
+                "regularBookdata/bookNoAuthor.csv",
+                "regularBookdata/bookRegular.csv",
+                "hardBackBookdata/hardBackNoAuthor.csv",
+                "hardBackBookdata/hardBackNoWeight.csv",
+                "eBookdata/eBook.csv",
+                "eBookdata/eBookNoAuthor.csv",
+                "audioBookdata/audioBook.csv",
+                "audioBookdata/audioBookNoBitrate.csv"
+        };
 
+        for (String relativePath : filePaths) {
+            String filePath = initialDirectory + relativePath;
+            parseCSV(filePath);
+        }
 
         return BookList;
     }
 
-    public ArrayList<Book> loadToList () {
-        int i = 1;
-        managebook.addBook(i++, "regular", "Inferno", "Dan Brown", "978-0385537858");
-        managebook.addBook(i++, "regular", "To Kill a Mockingbird", "978-0060935467");
-        managebook.addBook(i++, "regular", "1984", "George Orwell", "978-0451524935");
-        managebook.addBook(i++, "regular", "The Great Gatsby", "F. Scott Fitzgerald", "978-0743273565");
-        managebook.addBook(i++, "regular", "Pride and Prejudice", "Jane Austen", "978-1503290563");
-        managebook.addBook(i++, "regular", "The Catcher in the Rye", "J.D. Salinger", "978-0316769488");
-        managebook.addBook(i++, "regular", "The Hobbit", "978-0547928227");
-        managebook.addBook(i++, "regular", "Moby Dick", "978-1503280786");
-        managebook.addBook(i++, "regular", "War and Peace", "Leo Tolstoy", "978-1400079988");
-        managebook.addBook(i++, "regular", "Crime and Punishment", "Fyodor Dostoevsky", "978-0486415871");
-        managebook.addBook(i++, "regular", "The Odyssey", "Homer", "978-0140268867");
-        managebook.addBook(i++, "regular", "Brave New World", "978-0060850524");
-        managebook.addBook(i++, "regular", "The Lord of the Rings", "J.R.R. Tolkien", "978-0544003415");
-        managebook.addBook(i++, "regular", "Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "978-0590353427");
-        managebook.addBook(i++, "regular", "The Chronicles of Narnia", "C.S. Lewis", "978-0066238500");
-        managebook.addBook(i++, "regular", "Animal Farm", "978-0451526342");
-        managebook.addBook(i++, "regular", "The Book Thief", "Markus Zusak", "978-0375842207");
-        managebook.addBook(i++, "regular", "The Hunger Games", "Suzanne Collins", "978-0439023481");
-        managebook.addBook(i++, "regular", "The Kite Runner", "978-1594631931");
-        managebook.addBook(i++, "regular", "The Da Vinci Code", "Dan Brown", "978-0307474278");
+    private void parseCSV(String filePath) {
+        String line;
+        String csvSplitBy = ",";
 
-// Adding Hardbacks
-        managebook.addHardBack(new Book(i++,"Hardback", "To Kill a Mockingbird", "978-0060935467"),i,"Hardback", 384);
-        managebook.addHardBack(new Book(i++, "Hardback","The Hobbit", "978-0547928227"),i,"Hardback", "Leather", 310, 1.5);
-        managebook.addHardBack(new Book(i++, "hardBack", "Brave New World", "978-0060850524"),i,"Hardback",268);
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String header = br.readLine();
+            if (header == null) return;
 
-        managebook.addHardBack(new Book(i++, "Hardback", "Moby Dick", "978-1503280786"), i, "Hardback", "Hardcover", 635, 1.8);
-        managebook.addHardBack(new Book(i++, "Hardback", "War and Peace", "978-1400079988"), i, "Hardback", "Cloth", 1225, 2.5);
-        managebook.addHardBack(new Book(i++, "Hardback", "The Odyssey", "978-0140268867"), i, "Hardback", "Hardcover", 561, 1.7);
-        managebook.addHardBack(new Book(i++, "Hardback", "The Great Gatsby", "978-0743273565"), i, "Hardback", "Leather", 180, 0.5);
-        managebook.addHardBack(new Book(i++, "Hardback", "The Catcher in the Rye", "978-0316769488"), i, "Hardback", "Cloth", 214, 0.6);
+            String[] headers = header.split(csvSplitBy);
+            int headerLength = headers.length;
 
-// Adding EBooks
-        managebook.addEBook(new Book(i++, "eBook", "1984", "George Orwell", "978-0451524935")                       , i, "eBook", "PDF", 1.5);
-        managebook.addEBook(new Book(i++, "eBook", "Pride and Prejudice", "Jane Austen", "978-1503290563")          , i, "eBook", "EPUB", 2.1);
-        managebook.addEBook(new Book(i++, "eBook", "The Great Gatsby", "F. Scott Fitzgerald", "978-0743273565")     , i, "eBook", "MOBI", 1.2);
-        managebook.addEBook(new Book(i++, "eBook", "Moby Dick", "Herman Melville", "978-1503280786")                , i, "eBook", "PDF", 1.7);
-        managebook.addEBook(new Book(i++, "eBook", "War and Peace", "Leo Tolstoy", "978-1400079988")                , i, "eBook", "EPUB", 2.8);
-        managebook.addEBook(new Book(i++, "eBook", "Crime and Punishment", "Fyodor Dostoevsky", "978-0486415871")   , i, "eBook", "PDF", 1.6);
-        managebook.addEBook(new Book(i++, "eBook", "The Odyssey", "Homer", "978-0140268867")                        , i, "eBook", "EPUB", 1.9);
-        managebook.addEBook(new Book(i++, "eBook", "Brave New World", "Aldous Huxley", "978-0060850524")            , i, "eBook", "PDF", 1.4);
-        managebook.addEBook(new Book(i++, "eBook", "The Lord of the Rings", "J.R.R. Tolkien", "978-0544003415")     , i, "eBook", "MOBI", 3.5);
-        managebook.addEBook(new Book(i++, "eBook", "Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "978-0590353427"), i, "eBook", "EPUB", 2.3);
-        managebook.addEBook(new Book(i++, "eBook", "The Chronicles of Narnia", "C.S. Lewis", "978-0066238500")      , i, "eBook", "PDF", 1.8);
-        managebook.addEBook(new Book(i++, "eBook", "Animal Farm", "978-0451526342")                                        , i, "eBook", "MOBI", 1.2);
-        managebook.addEBook(new Book(i++, "eBook", "The Book Thief", "Markus Zusak", "978-0375842207")               , i, "eBook", "PDF", 1.7);
-        managebook.addEBook(new Book(i++, "eBook", "The Hunger Games", "Suzanne Collins", "978-0439023481")         , i, "eBook", "EPUB", 1.9);
+            while ((line = br.readLine()) != null) {
+                String[] bookData = line.split(csvSplitBy);
+                switch (headerLength) {
+                    case 4:
+                        if (headers[2].equals("author")) {
+                            managebook.addBook(BookList.size() + 1, bookData[0], bookData[1], bookData[2], bookData[3]);
+                        } else {
+                            managebook.addBook(BookList.size() + 1, bookData[0], bookData[1], bookData[2]);
+                        }
+                        break;
+                    case 5:
+                        if (headers[3].equals("pagecount")) {
+                            managebook.addHardBack(new Book(BookList.size() + 1, bookData[0], bookData[1], bookData[2]),
+                                    BookList.size() + 1, bookData[0], Integer.parseInt(bookData[3]));
+                        } else {
+                            managebook.addEBook(new Book(BookList.size() + 1, bookData[0], bookData[1], bookData[2]),
+                                    BookList.size() + 1, bookData[0], bookData[3]);
+                        }
+                        break;
+                    case 6:
+                        if (headers[4].equals("bookweight")) {
+                            managebook.addHardBack(new Book(BookList.size() + 1, bookData[0], bookData[1], bookData[2]),
+                                    BookList.size() + 1, bookData[0], bookData[3], Integer.parseInt(bookData[4]), Double.parseDouble(bookData[5]));
+                        } else {
+                            managebook.addEBook(new Book(BookList.size() + 1, bookData[0], bookData[1], bookData[2]),
+                                    BookList.size() + 1, bookData[0], bookData[4], Double.parseDouble(bookData[5]));
+                        }
+                        break;
+                    case 7:
+                        managebook.addAudioBook(new Book(BookList.size() + 1, bookData[0], bookData[1], bookData[2]),
+                                BookList.size() + 1, bookData[0], bookData[3], bookData[4], bookData[5]);
+                        break;
+                    case 8:
+                        managebook.addAudioBook(new Book(BookList.size() + 1, bookData[0], bookData[1], bookData[2]),
+                                BookList.size() + 1, bookData[0], bookData[3], Double.parseDouble(bookData[4]), bookData[5], bookData[6]);
+                        break;
+                }
+            }
 
-// Adding Audiobooks
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
-        managebook.addAudioBook(new Book(i++, "audioBook", "The Great Gatsby", "F. Scott Fitzgerald", "978-0743273565")                 , i, "audioBook", "MP3", 100.0, "AAC", "128kbps");
-        managebook.addAudioBook(new Book(i++, "audioBook", "Moby Dick", "Herman Melville", "978-1503280786")                            , i, "audioBook", "MP3", "AAC", "128kbps");
-        managebook.addAudioBook(new Book(i++, "audioBook", "Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "978-0590353427")   , i, "audioBook", "MP3", "AAC", "128kbps");
-        managebook.addAudioBook(new Book(i++, "audioBook", "The Hunger Games", "Suzanne Collins", "978-0439023481")                     , i, "audioBook", "MP3", "AAC", "128kbps");
-        managebook.addAudioBook(new Book(i++, "audioBook", "The Kite Runner", "978-1594631931")                                                , i, "audioBook", "MP3", "AAC", "96kbps");
-        managebook.addAudioBook(new Book(i++, "audioBook", "The Da Vinci Code", "Dan Brown", "978-0307474278")                          , i, "audioBook", "MP3", "AAC", "128kbps");
-        managebook.addAudioBook(new Book(i++, "audioBook", "Inferno", "Dan Brown", "978-0385537858")                                    , i, "audioBook", "MP3", "AAC", "192kbps");
-        managebook.addAudioBook(new Book(i++, "audioBook", "Brave New World", "978-0060850524")                                                 , i, "audioBook", "MP3", "AAC", "128kbps");
-        managebook.addAudioBook(new Book(i++, "audioBook", "1984", "George Orwell", "978-0451524935")                                   , i, "audioBook", "MP3", "AAC", "128kbps");
-        managebook.addAudioBook(new Book(i++, "audioBook", "The Chronicles of Narnia", "C.S. Lewis", "978-0066238500")                  , i, "audioBook", "MP3", "AAC", "96kbps");
-
-
-        return BookList;
-    };
 }
