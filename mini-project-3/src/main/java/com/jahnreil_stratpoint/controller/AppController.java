642fb326e91a79ef79ca8ff71e6e5c26226860bb
Jahnreil Amarillento <92299867+Mafunamiii@users.noreply.github.com>
2024-06-27 14:52:59 +0800

    Update loggers in appController
    added logging as part of appcontroller

--- mini-project-3/src/main/java/com/jahnreil_stratpoint/controller/AppController.java
+++ mini-project-3/src/main/java/com/jahnreil_stratpoint/controller/AppController.java
@@ -1,10 +1,16 @@
 package com.jahnreil_stratpoint.controller;
 
+import com.jahnreil_stratpoint.util.AppShutdownHook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * The main AppController to run E-Commerce application
  */
 public class AppController
 {
+    private static final Logger logger = LoggerFactory.getLogger(AppController.class);
+
     /**
      * The entry point of application.
      *
@@ -12,6 +18,15 @@ public class AppController
      */
     public static void main( String[] args )
     {
+        Runtime.getRuntime().addShutdownHook(new AppShutdownHook());
+
+        logger.info("Started main");
         ECommerceController eCommerceController = new ECommerceController();
+        logger.info("Created eCommerceController");
+        eCommerceController.startECommerceController();
+        logger.info("Started eCommerceController.startECommerceController()");
+
+
+        logger.info("Application Execution Terminated");
     }
 }
