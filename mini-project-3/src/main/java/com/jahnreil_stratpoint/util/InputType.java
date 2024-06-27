b76a7f1e32bbe67769ab828e1c292258d4d890b2
Jahnreil Amarillento <92299867+Mafunamiii@users.noreply.github.com>
2024-06-27 15:03:44 +0800

    Enum InputType
    enum creation and documentation

---
+++ mini-project-3/src/main/java/com/jahnreil_stratpoint/util/InputType.java
@@ -0,0 +1,19 @@
+package com.jahnreil_stratpoint.util;
+
+/**
+ * The enum Input type.
+ */
+public enum InputType {
+    /**
+     * Int with max choice input type.
+     */
+    INT_WITH_MAX_CHOICE,
+    /**
+     * Int only input type.
+     */
+    INT_ONLY,
+    /**
+     * To double input type.
+     */
+    TO_DOUBLE,
+}
