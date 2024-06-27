731d8222a44ac120ab6926db183404732dca3752
Jahnreil Amarillento <92299867+Mafunamiii@users.noreply.github.com>
2024-06-27 15:00:13 +0800

    Main menu updates
    added input validation

--- mini-project-3/src/main/java/com/jahnreil_stratpoint/controller/MainMenuController.java
+++ mini-project-3/src/main/java/com/jahnreil_stratpoint/controller/MainMenuController.java
@@ -1,24 +1,35 @@
 package com.jahnreil_stratpoint.controller;
 
+import com.jahnreil_stratpoint.util.InputCheck;
+import com.jahnreil_stratpoint.util.InputType;
 import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
 
 /**
- * The type Main menu controller.
+ * The type Main menu controller handles the main menu processes which returns an int value to ECommerceController
  */
 @NoArgsConstructor
 public class MainMenuController {
+    private static final Logger logger = LoggerFactory.getLogger(MainMenuController.class);
     /**
-     * Menu choice int.
-     *
-     * @return the int
+     * public scanner inputscan that can be used throughout the entire application
+     * */
+    public static Scanner inputscan = new Scanner(System.in);
+
+    /**
+     * Menu choice int
+     * @return the int menuChoiceResult
      */
     public int menuChoice() {
         int menuChoiceResult = 0;
-
+        logger.info("Started menuChoice()");
         while (true) {
-            System.out.println(
+            System.out.print(
                     """
-                    ==========================================
+                    \n==========================================
                     
                                     JG Market
                               [1] Browse Products
@@ -26,11 +37,19 @@ public class MainMenuController {
                               [3] Exit Program
                               
                     ==========================================
-                    ►""");
+                    ► """);
+            String input = inputscan.nextLine();
+            InputCheck inputCheck = InputCheck.inputCheckValues(input, 3, InputType.INT_WITH_MAX_CHOICE);
 
-            break;
+            if (inputCheck.isValid()) {
+                logger.info("menuChoice valid. Selected menu choice: {}", inputCheck.getValueInt());
+                menuChoiceResult = inputCheck.getValueInt();
+                break;
+            } else {
+                logger.warn("Invalid menuChoice input: {}", input);
+            }
         }
-
+        logger.info("Returning menuChoiceResult {}", menuChoiceResult);
         return menuChoiceResult;
     }
 }
