82f200564249990ec5aa3ad17befca5d2bc29fbe
Jahnreil Amarillento <92299867+Mafunamiii@users.noreply.github.com>
2024-06-27 15:27:45 +0800

    Added documentation

--- mini-project-3/src/main/java/com/jahnreil_stratpoint/util/AppShutdownHook.java
+++ mini-project-3/src/main/java/com/jahnreil_stratpoint/util/AppShutdownHook.java
@@ -4,11 +4,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * The type App shutdown hook.
+ * The type App shutdown hook serves as trigger to log info to logger that the application is shuting down
  */
 public class AppShutdownHook extends Thread{
     private static final Logger logger = LoggerFactory.getLogger(AppShutdownHook.class);
 
+    /**
+     * run() is overriden to enter log to logger
+     */
     @Override
     public void run() {
         logger.info("Application is shutting down");
