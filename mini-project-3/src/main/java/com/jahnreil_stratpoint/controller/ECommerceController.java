2b8483b51b540cc50da0020318b25fab5afa02cd
Jahnreil Amarillento <92299867+Mafunamiii@users.noreply.github.com>
2024-06-27 14:53:21 +0800

    Main Menu implementation switch cases

--- mini-project-3/src/main/java/com/jahnreil_stratpoint/controller/ECommerceController.java
+++ mini-project-3/src/main/java/com/jahnreil_stratpoint/controller/ECommerceController.java
@@ -1,12 +1,16 @@
 package com.jahnreil_stratpoint.controller;
 
 import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * The type E commerce controller.
  */
 @NoArgsConstructor
 public class ECommerceController {
+    private static final Logger logger = LoggerFactory.getLogger(MainMenuController.class);
+
     private static MainMenuController mainMenuController = new MainMenuController();
     private static ProductShelfController productShelfController = new ProductShelfController();
 
@@ -15,7 +19,22 @@ public class ECommerceController {
      */
     public void startECommerceController() {
         while (true) {
+            int mainMenuChoice = mainMenuController.menuChoice();
 
+            switch (mainMenuChoice) {
+                case 1:
+                    logger.info("View Product Shelf, main menu choice: {}", mainMenuChoice);
+
+                    break;
+                case 2:
+                    logger.info("View Cart, main menu choice: {}", mainMenuChoice);
+
+                    break;
+                case 3:
+                    logger.info("Exiting program, main menu choice: {}", mainMenuChoice);
+                    System.out.println("Exiting Program");
+                    return;
+            }
         }
     }
 }
